6a9789e03dd7a73fcde7953a0f12868925bca1f0
Jotar, Micael <jotarmicael@gmail.com>
2022-09-30 16:38:58 -0300

    add requirements.txt

--- creativescienceapp/admin.py
+++ creativescienceapp/admin.py
@@ -3,5 +3,10 @@ from creativescienceapp.models import Role,User
 
 # Register your models here.
 
-admin.site.register(Role)
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=("id","complete_name","username","email","password","verified","role_id")
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display=("id","name")
+admin.site.register(Role,RoleAdmin)
+admin.site.register(User,UserAdmin)
--- creativescienceapp/models.py
+++ creativescienceapp/models.py
@@ -3,12 +3,17 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    complete_name=models.CharField(max_length=30)
-    username=models.CharField(max_length=30)
-    email=models.EmailField(max_length=30)
-    password=models.CharField(max_length=30)
-    role_id=models.IntegerField()
-    verified=models.BooleanField(default=False)
+    complete_name=models.CharField(max_length=30,blank=False,null=False)
+    username=models.CharField(max_length=30,blank=False,null=False)
+    email=models.EmailField(max_length=30,blank=False,null=False)
+    password=models.CharField(max_length=30,blank=False,null=False)
+    role_id=models.IntegerField(blank=False,null=False)
+    verified=models.BooleanField(default=False,blank=False,null=False)
 
+    def __str__(self):
+        return (self.complete_name,self.username,self.email,self.password,self.verified,self.role_id)
 class Role(models.Model):
     name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return (self.name)
